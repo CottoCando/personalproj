ade7f7c51b34fae8682be30ae3b98feaeb3f16e1
CottoCando <minsukim006@gmail.com>
2024-06-17 17:40:07 +1000

    changed

--- yahheardd.py
+++ yahheardd.py
@@ -1,4 +1,11 @@
 #git pull update from git to pc
-#git add adds the things i want to update, in sections
+#git add adds the things i want to update, in sections. its in a list of things im putting
+#git commit -m. grouped up and added a message
+#git push
+#git pull
 
-print("hello world")
+
+i = 0 
+while i < 10:
+    print(i)
+    i += 1
